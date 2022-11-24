da9cd121ed154f005b606f89d15ae7d47a5ebc52
Gabrielle Richards <gabbynicole@comcast.net>
2022-11-24 08:34:32 -0500

    set initial auth status when page is first rendered

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
     const {logout} = useLogout()
+    const {user} = useAuthContext()
 
     const handleClick = () => {
         logout()
@@ -15,13 +17,18 @@ const Navbar = () => {
                     <h1>Healthology</h1>
                 </Link>
                 <nav>
+                    {user && (
                     <div>
+                        <span>{user.email}</span>
                         <button onClick={handleClick}>Log out</button>
                     </div>
+                    )}
+                    {!user && (
                     <div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Sign up</Link>
                     </div>
+                    )}
                 </nav>
             </div>
         </header>
