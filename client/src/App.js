42a4bd0a280ec521379a4b40437a66ed4afee8cd
Gabrielle Richards <gabbynicole@comcast.net>
2022-11-24 14:30:12 -0500

    protected react routes
    -users can only access the homepage if they are logged in
    -if users log in or sign up, they are redirected to the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 import Home from './pages/Home'
 import Appbar from "./Appbar"
@@ -9,6 +10,7 @@ import Logon from './pages/Logon'
 import Signup2 from './pages/Signup2'
 
 function App() {
+  const {user} = useAuthContext()
   return (
     <div className="App">  
       <BrowserRouter>
@@ -17,15 +19,15 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={<Home />}
+              element={user ? <Home /> : <Navigate to= "/login" />}
               />
               <Route
               path="/login"
-              element={<Login />}
+              element={!user ? <Login /> : <Navigate to="/" />}
               />
               <Route
               path="/signup"
-              element={<Signup />}
+              element={!user ? <Signup /> : <Navigate to="/" />}
               />
           </Routes>
         </div>
