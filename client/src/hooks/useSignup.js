f7e4d85806cccb3daea6f787f35a8c50ca0bfea2
Gabrielle Richards <gabbynicole@comcast.net>
2022-11-23 19:16:59 -0500

    create signup hook

---
+++ client/src/hooks/useSignup.js
@@ -0,0 +1,34 @@
+import  { useState } from 'react'
+import { useAuthContext } from './useAuthContext'
+
+export const useSignup = () => {
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const { dispatch } = useAuthContext()
+
+    const signup = async (email, password) => {
+        setIsLoading(true)
+        setError(null)
+
+        const response = await fetch('/api/user/signup', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email, password})
+        })
+        const json = await response.json()
+
+        if (!response.ok) {
+            setIsLoading(false)
+            setError(json.error)
+        }
+        if (response.ok){
+            //if user exits page, make it so they can get back in with jwt
+            localStorage.setItem('user', JSON.stringify(json))
+
+            dispatch({type: 'LOGIN', payload: json})
+
+            setIsLoading(false)
+        }
+    }
+    return {signup, isLoading, error}
+} 
