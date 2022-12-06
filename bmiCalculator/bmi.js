c265f66c5289759ff7ece74acb0ffb319bc2625d
cathylawr12 <cathylawr12@gmail.com>
2022-12-05 21:13:17 -0500

    lessened complexity of bmi.js

--- bmiCalculator/bmi.js
+++ bmiCalculator/bmi.js
@@ -15,29 +15,27 @@ function calculateBMI(){
     else if(weight == "" || weight <= 0 || isNaN(weight)){
         result.innerHTML = "Provide a valid weight in lbs."
     }
-    else{
-        let bmi = ((weight/2.205)/((height*height)/10000)).toFixed(2)
-        if(bmi < 16.00){
-            result.innerHTML = `Severely Underweight: <span>${bmi}</span>`
-        }
-        else if(bmi >= 16.0 && bmi <= 18.4){
-            result.innerHTML = `Underweight: <span>${bmi}</span>`
-        }
-        else if(bmi >= 18.5 && bmi <= 24.9){
-            result.innerHTML = `Normal: <span>${bmi}</span>`
-        }
-        else if(bmi >= 25.0 && bmi <= 29.9){
-            result.innerHTML = `Overweight: <span>${bmi}</span>`
-        }
-        else if(bmi >= 30.0 && bmi <= 34.9){
-            result.innerHTML = `Moderately Obese: <span>${bmi}</span>`
-        }
-        else if(bmi >= 35.0 && bmi <= 39.9){
-            result.innerHTML = `Severely Obsese: <span>${bmi}</span>`
-        }
-        else if(bmi >= 40.0){
-            result.innerHTML = `Morbidly Obese: <span>${bmi}</span>`
-        }
+    let bmi = ((weight/2.205)/((height*height)/10000)).toFixed(2)
+    if(bmi < 16.00){
+        result.innerHTML = `Severely Underweight: <span>${bmi}</span>`
+    }
+    else if(bmi <= 18.4){
+        result.innerHTML = `Underweight: <span>${bmi}</span>`
+    }
+    else if(bmi <= 24.9){
+        result.innerHTML = `Normal: <span>${bmi}</span>`
+    }
+    else if(bmi <= 29.9){
+        result.innerHTML = `Overweight: <span>${bmi}</span>`
+    }
+    else if(bmi <= 34.9){
+        result.innerHTML = `Moderately Obese: <span>${bmi}</span>`
+    }
+    else if(bmi <= 39.9){
+        result.innerHTML = `Severely Obsese: <span>${bmi}</span>`
+    }
+    else {
+        result.innerHTML = `Morbidly Obese: <span>${bmi}</span>`
     }
 }
 
