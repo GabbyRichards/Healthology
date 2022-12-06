96b320508844c641efce1938a243334aef4c5858
cathylawr12 <cathylawr12@gmail.com>
2022-12-05 22:13:54 -0500

    Update bmi.js

--- bmiCalculator/bmi.js
+++ bmiCalculator/bmi.js
@@ -1,21 +1,32 @@
+/*
+here is the signal for the function to start. 
+It will detect the button from index.html being clicked 
+*/
 window.onload = () => {
     let button = document.querySelector("#btn")
     button.addEventListener("click", calculateBMI)
 }
 
+//this is the function that wil calculate the bmi of a user
 function calculateBMI(){
+    //these are the height and weight variables that are used for the Bmi formula
     let height = parseInt(document.querySelector("#height").value)
     let weight = parseInt(document.querySelector("#weight").value)
 
+    //this is the variable that represents the result displayed on the screen
     let result = document.querySelector("#result")
 
+    //this will check whether the height and weight values are valid
     if(height == "" || height <= 0 || isNaN(height)){
         result.innerHTML = "Provide a valid height in cm."
     }
     else if(weight == "" || weight <= 0 || isNaN(weight)){
         result.innerHTML = "Provide a valid weight in lbs."
     }
+    //this is the calculated bmi 
     let bmi = ((weight/2.205)/((height*height)/10000)).toFixed(2)
+    //the following if-statements will check the range that the bmi falls into
+    //and will tell the user where their bmi lies in (the category)
     if(bmi < 16.00){
         result.innerHTML = `Severely Underweight: <span>${bmi}</span>`
     }
